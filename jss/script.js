0f40a8efd14734ab0156fae9802e661d9fb4e784
nastiakinlem <anastasiia07kinlem@gmail.com>
2022-09-01 17:17:13 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ jss/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $( function() {
+        $( "#dialog" ).dialog({
+            autoOpen: false,
+            show: {
+                effect: "blind",
+                duration: 1000
+            },
+            hide: {
+                effect: "explode",
+                duration: 1000
+            }
+        });
+
+        $( "#opener" ).on( "click", function() {
+            $( "#dialog" ).dialog( "open" );
+        });
+    } );
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
